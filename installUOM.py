555fb87adeb19a7296d96030d6f34d6e0d40087f
konkos <k@k.com>
2022-03-28 02:41:58 +0300

    added SYSTEM TYPE detection

--- installUOM.py
+++ installUOM.py
@@ -1,26 +1,34 @@
 import os
+from LinuxDistrosEnum import LinuxDistro
 
-def createPackageManagerListFiles():
+
+def create_package_manager_list_files():
     os.system("which apt > apt.txt")
     os.system("which dnf > dnf.txt")
 
-def removePackageManagerListFiles():
+
+def remove_package_manager_list_files():
     os.system("rm apt.txt")
     os.system("rm dnf.txt")
 
 
-if __name__=="__main__":
+SYSTEM_TYPE = ""
 
-    createPackageManagerListFiles()
-        
+if __name__ == "__main__":
+
+    create_package_manager_list_files()
+
     with open("apt.txt", "r") as f:
         lines = f.readlines()
         if len(lines) > 0:
-            os.system("bash ./installUOM_DEBIAN.sh")
+            # os.system("bash ./installUOM_DEBIAN.sh")
+            SYSTEM_TYPE = LinuxDistro.APT
 
     with open("dnf.txt", "r") as f:
         lines = f.readlines()
         if len(lines) > 0:
-            os.system("bash ./installUOM_RPM.sh")
+            # os.system("bash ./installUOM_DNF.sh")
+            SYSTEM_TYPE = LinuxDistro.DNF
 
-    removePackageManagerListFiles()
+    print(SYSTEM_TYPE)
+    # remove_package_manager_list_files()
