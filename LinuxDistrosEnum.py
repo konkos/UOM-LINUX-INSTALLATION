fbabf803ed488a5a017ec91ed9bfe0f01ce87059
kon_k <kon_kostas@windowslive.com>
2022-04-03 14:03:55 +0300

    small changes

--- LinuxDistrosEnum.py
+++ LinuxDistrosEnum.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class LinuxDistro(Enum):
+    NOT_FOUND = 0
     RPM = 1
     DNF = 2
-    APT = 2
+    APT = 3
--- installUOM_DNF.sh
+++ installUOM_DNF.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "RPM"
+#add dnf install script
+#eg sudo dnf install program1, program2, ...
+echo "dnf"
